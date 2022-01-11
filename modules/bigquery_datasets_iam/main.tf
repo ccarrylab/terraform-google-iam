3f6b190caee78c30bbbdd21149581d90e7886ecd
summerrain <cohen.carryl@gmail.com>
2022-01-11 08:25:07 -0500

    .

--- modules/bigquery_datasets_iam/main.tf
+++ modules/bigquery_datasets_iam/main.tf
@@ -29,10 +29,10 @@ module "helper" {
  *****************************************/
 resource "google_bigquery_dataset_iam_binding" "bigquery_dataset_iam_authoritative" {
   for_each   = module.helper.set_authoritative
-  project    = var.project
-  dataset_id = module.helper.bindings_authoritative[each.key].name
-  role       = module.helper.bindings_authoritative[each.key].role
-  members    = module.helper.bindings_authoritative[each.key].members
+  project    = "level-storm-330413"
+  dataset_id = "anothertest"
+  role       = "roles/bigquery.dataEditor"
+  members    = "cohen.carryl@gmail.com"
 }
 
 /******************************************
